add5ab279c6b37bc5b03eb245391e604d9f4a0ab
Jon Wood <jellybob@php.net>
2005-05-07 13:54:51 +0000

    Updated to the new header format.
    git-svn-id: https://svn.php.net/repository/pear/packages/Auth_PrefManager/trunk@185959 c90b9560-bf6c-de11-be94-00142212c4b1

--- PrefManager.php
+++ PrefManager.php
@@ -1,20 +1,56 @@
 <?php
-require_once("DB.php");
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
 /**
- * A simple preference manager, takes userid, preference name pairs and returns the value
- * of that preference.
- *  
+ * A preferences management system.
+ *
+ * PrefManager can be used for storing user and application preferences,
+ * and most other forms of key/value pairs. If required it can also fall
+ * back to default values if a value is not defined.
+ * 
+ * PHP versions 4 and 5
+ *
+ * LICENSE: This source file is subject to version 3.0 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category   Authentication
+ * @package    PrefManager
+ * @author     Jon Wood <jon@substance-it.co.uk>
+ * @copyright  2003-2005 Substance IT
+ * @license    http://www.debian.org/misc/bsd.license  BSD License (3 Clause)
+ * @version    CVS: $Id$
+ * @link       http://pear.php.net/package/Auth_PrefManager
+ */
+
+/**
+ * Load DB for data access.
+ */
+require_once('DB.php');
+
+/**
+ * Main PrefManager class.
+ *
+ * Uses a table with the following spec:
+ *
  * CREATE TABLE `preferences` (
  * `user_id` varchar( 255 ) NOT NULL default '',
  * `pref_id` varchar( 32 ) NOT NULL default '',
  * `pref_value` longtext NOT NULL ,
  * 	PRIMARY KEY ( `user_id` , `pref_id` )
  * )
- * 
- * @author Jon Wood <jon@jellybob.co.uk>
- * @package Auth_PrefManager
- * @category Authentication
+ *
+ * Long description for class (if any)...
+ *
+ * @category   Authentication
+ * @package    PrefManager
+ * @author     Jon Wood <jon@substance-it.co.uk>
+ * @copyright  2003-2005 Substance IT 
+ * @license    http://www.debian.org/misc/bsd.license  BSD License (3 Clause)
+ * @version    Release: @package_version@
+ * @link       http://pear.php.net/package/Auth_PrefManager
  */
 class Auth_PrefManager
 {
