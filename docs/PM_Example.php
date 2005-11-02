92debd490fa5f48096d8976d8b0ebb2888478359
Christophe Gesche <moosh@php.net>
2005-11-02 12:55:34 +0000

    - parse error
    git-svn-id: https://svn.php.net/repository/pear/packages/Auth_PrefManager/trunk@199775 c90b9560-bf6c-de11-be94-00142212c4b1

--- docs/PM_Example.php
+++ docs/PM_Example.php
@@ -2,13 +2,13 @@
     require_once("Auth/PrefManager.php");
     $options = array("table" => "user_prefs");
     $prefs = new PrefManager($dsn, $options);
-    
+
     $prefs->setDefaultValue("fullname", "New User");
     $prefs->setValue("jon", "fullname", "Jon Wood");
-    
+
     $jonName = $prefs->getPref("jon", "fullname");
-    $billName = $prefs->getPref("bill", "")
-    
+    $billName = $prefs->getPref("bill", "");
+
     /*
     * Outputs:
     * <p>Jon's name is Jon Wood<br/>
